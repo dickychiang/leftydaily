28351bb4ff4391428cb896700baaaf5c9915ba4d
hychiang <dickychiang73@gmail.com>
2016-12-24 22:10:07 +0800

    setup server

--- server.js
+++ server.js
@@ -1,6 +1,23 @@
 var express = require("express");
+var bodyParser = require('body-parser');
+
 var app = express();
 var port = process.env | 3000;
+
+// parse the engine to HTML
+app.engine('html', require('ejs').renderFile);
+
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+app.use(bodyParser.json());
+
+app.set('view engine', 'html');
+
+// set router
+app.get('/', function(req, res){
+		res.render('index.html');
+});
 
 app.listen(port, function(){
 	console.log('Express started on http://localhost:' + port);
